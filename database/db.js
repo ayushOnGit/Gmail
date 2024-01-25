65b69774ac5bbaad4d8d4f79f43a4866f97df862
Ayush <ayushsingh1771@gmail.com>
2024-01-25 23:05:26 +0530

    enc-check

--- database/db.js
+++ database/db.js
@@ -1,7 +1,10 @@
 const mongoose = require('mongoose');
+const dotenv = require('dotenv')
+
+dotenv.config();
 
 const Connections = async () => {
-    const DB_URI = `mongodb://gmailClone:7291019063@ac-spnm3qa-shard-00-00.f550bnp.mongodb.net:27017,ac-spnm3qa-shard-00-01.f550bnp.mongodb.net:27017,ac-spnm3qa-shard-00-02.f550bnp.mongodb.net:27017/?ssl=true&replicaSet=atlas-k2h3ce-shard-0&authSource=admin&retryWrites=true&w=majority`; // Your MongoDB connection URI
+    const DB_URI = `mongodb://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@ac-spnm3qa-shard-00-00.f550bnp.mongodb.net:27017,ac-spnm3qa-shard-00-01.f550bnp.mongodb.net:27017,ac-spnm3qa-shard-00-02.f550bnp.mongodb.net:27017/?ssl=true&replicaSet=atlas-k2h3ce-shard-0&authSource=admin&retryWrites=true&w=majority`; // Your MongoDB connection URI
     try {
         await mongoose.connect(DB_URI, {
             useNewUrlParser: true,
